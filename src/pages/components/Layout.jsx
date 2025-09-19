54f557f63be658b36909d136e98c76dcb7edb018
unknown <ariferdiansah1825@gmail.com>
2025-09-19 17:30:48 +0700

    perbaikan ukuran retur

--- src/pages/components/Layout.jsx
+++ src/pages/components/Layout.jsx
@@ -34,8 +34,8 @@ const modalStyles = {
   returContent: {
     backgroundColor: 'transparent', 
     borderRadius: '16px',
-    width: '100%',
-    maxWidth: '1000px', 
+    width: '90%',
+    maxWidth: '1300px', 
     height: '95vh',
     display: 'flex',
     flexDirection: 'column',
